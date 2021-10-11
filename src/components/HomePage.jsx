1c6f956a17f76fc2e1467c0b0ac1b13b40e272f4
Wilder David Serna Montoya <wildser@gmail.com>
2021-10-11 15:08:04 -0500

    arreglando archivos

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,21 +1,18 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Redirect} from 'react-router'
-import { apiFetch } from '../functions/fetch';
+//import { apiFetch } from '../functions/fetch';
 import HomeCarousel from './HomeCarousel';
 import Staticbar from './Staticbar';
 
-<<<<<<< HEAD
 
 
-=======
->>>>>>> 8b9908e04e234d8ebfef8f0c6f1ce0b490f9550f
 
 function HomePage() {
     
     const clave = sessionStorage.key(0);
-    console.log(clave);
+  
     //const userName = JSON.parse(sessionStorage.getItem(clave))
-    useEffect( ()=>apiFetch())
+    //useEffect( ()=>apiFetch())
     //const userNameActual =userName.displayName
   
     return (
